15897ec0784a060821413d93661398f8cf95f02a
Daniel <danielceballos94@gmail.com>
2020-01-29 11:21:43 -0500

    Add menu

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Menu } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Menu stackable>
+        <Menu.Item style={{ flexGrow: '1' }}>
+          <img src="https://react.semantic-ui.com/logo.png" alt="placeholder" />
+        </Menu.Item>
+        <Menu.Item name="home" link as={Link} to="/">
+          Home
+        </Menu.Item>
+        <Menu.Item name="sign-in" link as={Link} to="/login">
+          Search
+        </Menu.Item>
+      </Menu>
+    );
+  }
+}
+
+export default Header;
