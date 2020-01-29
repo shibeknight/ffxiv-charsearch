b968d215325a25e1f35242cbecbee213ffd1780e
Daniel <danielceballos94@gmail.com>
2020-01-29 13:50:12 -0500

    Add search form and list using Feed

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Header, Button, Segment, Image, Form } from 'semantic-ui-react';
+import { Grid, Header, Button, Segment, Image, Form, Loader, Feed, Card } from 'semantic-ui-react';
 import api from '../utils/API';
 
 class Login extends Component {
@@ -12,17 +12,19 @@ class Login extends Component {
       itemsTotal: '',
       pages: '',
       searchValue: '',
-      isLoading: true,
+      isLoading: false,
+      isReady: false,
     };
   }
 
   searchItems = async value => {
+    console.log(value);
     const chars = await api.searchChars(value);
     console.log(chars);
     this.setState({
-      items: chars.data.results,
-      itemsTotal: chars.data.count,
+      items: chars.data.Results,
       isLoading: false,
+      isReady: true,
     });
   };
 
@@ -37,7 +39,52 @@ class Login extends Component {
   };
 
   render() {
-    const { items, active, pages, isLoading, searchValue } = this.state;
+    const { items, active, pages, isLoading, searchValue, isReady } = this.state;
+    let section;
+    if (isLoading) {
+      section = <Loader active size="massive" content="Loading..." />;
+    } else if (isReady) {
+      section = (
+        <Feed size="large">
+          {items.map(character => (
+            <Feed.Event>
+              <Feed.Label>
+                <img
+                  src="https://react.semantic-ui.com/images/avatar/small/elliot.jpg"
+                  alt="profile"
+                />
+              </Feed.Label>
+              <Feed.Content>
+                <Feed.Summary>{character.Name}</Feed.Summary>
+              </Feed.Content>
+            </Feed.Event>
+          ))}
+        </Feed>
+      );
+    } else {
+      section = (
+        <>
+          <Header as="h2" color="teal" textAlign="center">
+            <Image src="https://react.semantic-ui.com/logo.png" /> Search for your character
+          </Header>
+          <Form size="large" onSubmit={this.handleSubmit}>
+            <Segment style={{ border: 'none', boxShadow: 'none' }}>
+              <Form.Input
+                value={searchValue}
+                onChange={this.handleSearchChange}
+                fluid
+                icon="user"
+                iconPosition="left"
+                placeholder="Character name"
+              />
+              <Button type="submit" color="teal" fluid size="large">
+                Search
+              </Button>
+            </Segment>
+          </Form>
+        </>
+      );
+    }
 
     return (
       <div>
@@ -51,25 +98,13 @@ class Login extends Component {
           }}
           verticalAlign="middle"
         >
-          <Grid.Column style={{ maxWidth: '450px' }}>
-            <Header as="h2" color="teal" textAlign="center">
-              <Image src="https://react.semantic-ui.com/logo.png" /> Search for your character
-            </Header>
-            <Form size="large" onSubmit={this.handleSubmit}>
-              <Segment>
-                <Form.Input
-                  value={searchValue}
-                  onChange={this.handleSearchChange}
-                  fluid
-                  icon="user"
-                  iconPosition="left"
-                  placeholder="Character name"
-                />
-                <Button type="submit" color="teal" fluid size="large">
-                  Search
-                </Button>
-              </Segment>
-            </Form>
+          <Grid.Column style={{ maxWidth: '450px', maxHeight: '450px', overflow: 'auto' }}>
+            <Card
+              raised
+              style={{ width: 'inherit', minHeight: '300px', justifyContent: 'space-evenly' }}
+            >
+              {section}
+            </Card>
           </Grid.Column>
         </Grid>
       </div>
