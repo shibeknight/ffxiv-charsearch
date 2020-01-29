05c03a6d7ca6ba0415932ce19aaa7cc2f5dfa054
Daniel <danielceballos94@gmail.com>
2020-01-29 11:19:50 -0500

    Wrap App in layout

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import { Switch, Route } from 'react-router-dom';
 import Login from './views/Login';
 import Home from './views/Home';
 import './App.css';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route exact path="/" component={Login} />
-        <Route exact path="/home" component={Home} />
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/home" component={Home} />
+        </Switch>
+      </Layout>
     </div>
   );
 }
