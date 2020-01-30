abfe2178625a375bc05cbc1f43059ac91f9abc26
Daniel <danielceballos94@gmail.com>
2020-01-30 13:08:09 -0500

    Add placeholder card

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Segment, Image, Header, Container } from 'semantic-ui-react';
+import { Grid, Segment, Image, Header, Container, Card, Icon } from 'semantic-ui-react';
 
 class Home extends Component {
   constructor(props) {
@@ -16,9 +16,26 @@ class Home extends Component {
         </Header>
         <Grid stackable columns={2}>
           <Grid.Column mobile={16} tablet={10} computer={10}>
-            <Segment>
-              <Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
-            </Segment>
+            <Card>
+              <Image
+                src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
+                wrapped
+                ui={false}
+              />
+              <Card.Content>
+                <Card.Header>Matthew</Card.Header>
+                <Card.Meta>
+                  <span className="date">Joined in 2015</span>
+                </Card.Meta>
+                <Card.Description>Matthew is a musician living in Nashville.</Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                <a>
+                  <Icon name="user" />
+                  22 Friends
+                </a>
+              </Card.Content>
+            </Card>
           </Grid.Column>
           <Grid.Column mobile={16} tablet={6} computer={6}>
             <Segment>
