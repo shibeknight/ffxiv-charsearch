2c71bf8b8dfffb0e385b496d136c4513b6349e3d
Daniel <danielceballos94@gmail.com>
2020-01-30 13:03:09 -0500

    Add container and base grid layout

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Grid, Segment, Image, Header, Container } from 'semantic-ui-react';
 
 class Home extends Component {
   constructor(props) {
@@ -8,7 +9,25 @@ class Home extends Component {
   }
 
   render() {
-    return <div>Home</div>;
+    return (
+      <Container>
+        <Header as="h2" color="teal">
+          Character Home
+        </Header>
+        <Grid stackable columns={2}>
+          <Grid.Column mobile={16} tablet={10} computer={10}>
+            <Segment>
+              <Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
+            </Segment>
+          </Grid.Column>
+          <Grid.Column mobile={16} tablet={6} computer={6}>
+            <Segment>
+              <Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
+            </Segment>
+          </Grid.Column>
+        </Grid>
+      </Container>
+    );
   }
 }
 
