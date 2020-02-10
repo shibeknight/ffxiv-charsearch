61631c6dc3cf7830058d85e59abbd607bde7384e
Daniel <danielceballos94@gmail.com>
2020-02-10 11:01:33 -0500

    Add placeholder grid for jobs

--- src/components/CharTabs.js
+++ src/components/CharTabs.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import { Tab } from 'semantic-ui-react';
+import { Tab, Container, Grid, Segment, Image } from 'semantic-ui-react';
 
 const panes = [
   {
     menuItem: 'Jobs',
-    render: () => <Tab.Pane attached={false}>Tab 1 Content</Tab.Pane>,
+    render: () => (
+      <Tab.Pane attached={false}>
+        <Container>
+          <Grid stackable columns={3}>
+            <Grid.Column>
+              <Segment>
+                <Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
+              </Segment>
+            </Grid.Column>
+          </Grid>
+        </Container>
+      </Tab.Pane>
+    ),
   },
   {
     menuItem: 'Free Company',
